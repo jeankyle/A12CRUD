9ed71b4d4d4d47cc18f81a6fbaf1870edd3adeb6
John Andrew <47737361+jeankyle@users.noreply.github.com>
2024-07-28 13:04:35 +0300

    upto components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // styleUrl: './app.component.css',
+  styleUrls: ['../assets/plugins/fontawesome-free/css/all.min.css', '../assets/css/adminlte.min.css', '../assets/css/custom.css', './app.component.css',]
 })
 export class AppComponent {
   title = 'Rahisi Solution';
